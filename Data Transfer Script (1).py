b5c40a180835657dabed8618e15112aaf6a40740
AkachukwuEkwueme <121521135+AkachukwuEkwueme@users.noreply.github.com>
2022-12-29 16:09:50 -0500

    Add files via upload

--- Data Transfer Script (1).py
+++ Data Transfer Script (1).py
@@ -8,13 +8,16 @@ import enum
 
 class ExcelWriter:
 
-   #global file_name
+#define a method for creating a workbook using the imported method, setting it active, and creating a worksheet
     
     def __init__(self, file_name):
         self.file_name = file_name
         self.workbook = Workbook()
         self.worksheet = self.workbook.active
         self.worksheet.title = file_name
+
+#define a method for writing a header and subheader starting on the first two empty rows and merges the two rows together. 
+#Method also sets fonts to specificed size, boldness, and name using imported module, as well as aligns center
 
     def write_header(self, header, subheader):
         self.worksheet.merge_cells(start_row=1, start_column=1, end_row=1, end_column=len(header))
@@ -40,11 +43,13 @@ class ExcelWriter:
 
     def save(self):
         self.workbook.save(self.file_name)
+#This method simply saves the adjusted workbook
 
     def does_file_exist(file_name):
         return os.path.isfile(file_name)
+#This method checks if a file exists with given name
 
-# Example usage
+# Example usage using a created array to transfer this automatically to an excel worksheet
 data = [
     ['1st Trial', '2nd Trial', '3rd Trial', '4th Trial', '5th Trial'],
     [1, 15, 202, 240, 59],
@@ -62,6 +67,8 @@ data = [
     #last_row = worksheet.max_row
     # Append the data to the worksheet
 
+    
+#calling the methods above with the given file/workbook name, and given header and subheader titles, then saves workbook
 excel_writer = ExcelWriter("datafile.xlsx")
 excel_writer.write_header('Force', 'N')
 excel_writer.write_data(data)
